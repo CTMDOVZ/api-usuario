a4d0998faa3225f998cdb7a835f296672436859e
Mateo Espinoza Monti <132113148+CTMDOVZ@users.noreply.github.com>
2024-11-28 08:35:52 -0500

    Update UpdateUsuario.py

--- UpdateUsuario.py
+++ UpdateUsuario.py
@@ -1,28 +1,37 @@
 import boto3
+import json
 
-def update_usuario(event, context):
-    # Obtener el ID del usuario y los atributos a actualizar
-    user_id = event['user_id']
-    atributos_actualizar = event['atributos']  # Diccionario con los atributos y valorefdfds a actualizar
+def lambda_handler(event, context):
+    try:
+        tenant_id = event['pathParameters']['tenant_id']
+        user_id = event['pathParameters']['user_id']
+        body = json.loads(event.get('body', '{}'))
 
-    # Conectar con DynamoDB
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('t_usuarios')
+        # Conectar a DynamoDB
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('t_usuarios')
 
-    # Construir la expresión de actualización
-    update_expression = "SET " + ", ".join([f"{k} = :{k}" for k in atributos_actualizar.keys()])
-    expression_attribute_values = {f":{k}": v for k, v in atributos_actualizar.items()}
+        # Actualizar los atributos del usuario
+        table.update_item(
+            Key={
+                'tenant_id': tenant_id,
+                'user_id': user_id
+            },
+            UpdateExpression="set email = :email, password = :password",
+            ExpressionAttributeValues={
+                ':email': body['email'],
+                ':password': body['password']
+            }
+        )
 
-    # Actualizar el ítem
-    response = table.update_item(
-        Key={
-            'user_id': user_id
-        },
-        UpdateExpression=update_expression,
-        ExpressionAttributeValues=expression_attribute_values
-    )
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'message': 'Usuario actualizado con éxito'})
+        }
 
-    return {
-        'statusCode': 200,
-        'body': f'Usuario con user_id={user_id} actualizado correctamente.'
-    }
+    except Exception as e:
+        print(f"Exception: {str(e)}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': 'Internal server error', 'details': str(e)})
+        }
